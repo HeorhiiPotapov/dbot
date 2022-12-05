1cc6c0e66ef5a992731c3d90b6925875a9e1a3dd
HeorhiiPotapov <bayjel13@gmail.com>
2022-12-05 11:26:14 +0200

    Plots building functionality added

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-""" main configuration file represents a sterter point of the application
+""" The main project's file, used to startup application
+
+    #! Configue watcher to have hot-reloading functionality:
+
+    #? example placed bellow can be added to the client's on_ready event handler:
+        from cogwatch import Watcher
+
+        async def on_ready():
+            watcher = Watcher(client, path='.', preload=True)
+            await watcher.start()
+
+    #? Also there's a way to use @watch decorator:
+        from cogwatch import watch
+        from discord.ext import commands
+
+        class ExampleBot(commands.Bot):
+            def __init__(self):
+                super().__init__(command_prefix='!')
+
+            @watch(path='commands')
+            async def on_ready():
+                print('Bot started success...')
 """
+
 from typing import Any
 from os import getenv
 from dotenv import load_dotenv
@@ -13,18 +35,12 @@ from statsmodels.tsa.arima.model import ARIMA
 
 load_dotenv()
 
-handler = FileHandler(
-    filename='discord.log',
-    encoding='utf-8',
-    mode='w',
-)
-
 intents = Intents.default()
 intents.message_content = True
 client = Client(intents=intents)
 
 
-def attachment_to_plot(file_name):
+def read_attachment(file_name):
     PARAMS = {
         'filepath_or_buffer': file_name,
         'header': 0,
@@ -32,18 +48,32 @@ def attachment_to_plot(file_name):
         'index_col': 0,
         # 'date_parser': lambda x: datetime.strptime('200'+x, '%Y-%m' )
     }
-    series = read_csv(**PARAMS)
+    return read_csv(**PARAMS)
+
+
+def attachment_summary(file_name):
+    series = read_attachment(file_name)
     series.index = series.index.to_period('M')
     model = ARIMA(series, order=(5, 1, 0))
     model_fit = model.fit()
     return model_fit.summary()
 
 
+def plot_dataframe(df, title="", dpi=100):
+    pyplot.figure(figsize=(15,4), dpi=dpi)
+    pyplot.plot(df, color='tab:red')
+    pyplot.gca().set(title=title)
+    # pyplot.show()
+    pyplot.imsave(fname=title+'.png')
+    import os
+    return title+'.png'
+
+
 @client.event
 async def on_ready() -> None:
     """Startup event handler
     """
-    print(f'Our client have logged in as {client.user}')
+    print(f"Our client have logged in as {client.user}")
 
 
 @client.event
@@ -55,8 +85,21 @@ async def on_message(message: Message) -> Any:
 
     if message.attachments[0]:
         file_name: str = message.attachments[0].filename
-        await message.attachments[0].save(file_name)
-        await message.channel.send(attachment_to_plot(file_name))
+
+        await message.attachments[0].save(
+            fp=file_name,
+            use_cached=True
+        )
+
+        await message.channel.send(
+            plot_dataframe(
+                df=read_attachment(file_name),
+                title=file_name,
+            )
+        )
+        await message.channel.send(
+            attachment_summary(file_name)
+        )
 
     if message.content.startswith('$hi'):
         await message.channel.send("Hi there! What's up?")
@@ -65,11 +108,8 @@ async def on_message(message: Message) -> Any:
 def run() -> None:
     """startup MyClient
     """
-    client.run(
-        getenv('DISCORD_TOKEN'),
-        log_handler=handler,
-        log_level=DEBUG,
-    )
+    handler = FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    client.run(getenv('DISCORD_TOKEN'), log_handler=handler, log_level=DEBUG)
 
 
 if __name__ == '__main__':
